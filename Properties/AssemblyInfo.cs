d80befb5867934917fa64a8686cc86cbbfa94f66
DeathWeasel1337 <deathweasel1337@gmail.com>
2019-10-21 14:06:19 -0500

    Add project

---
+++ Properties/AssemblyInfo.cs
@@ -0,0 +1,16 @@
+﻿using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("EnableResize")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("EnableResize")]
+[assembly: AssemblyCopyright("Copyright ©  2019")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: AssemblyVersion(EnableResize.Version)]
+[assembly: AssemblyFileVersion(EnableResize.Version)]
